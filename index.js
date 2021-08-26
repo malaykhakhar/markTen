655f9d00faf914b79bd867c919866cc5ecbd5783
Malay Khakhar <64819367+malaykhakhar@users.noreply.github.com>
2021-08-26 12:45:53 +0530

    docs: added index.js

--- index.js
+++ index.js
@@ -3,20 +3,29 @@ const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const errorMessage = document.querySelector("#error");
 
+const availableNotes = ["2000", "500", "100", "20", "10", "5", "1"];
+
+var noOfNotes = document.querySelectorAll(".no-of-notes");
+
 checkButton.addEventListener("click", validateBillAndCash);
 
-function validateBillAndCash() {
+function calculate(amount) {
+  var notes;
+  for (var i = 0; i < availableNotes.length; i++) {
+    notes = Math.trunc(amount / availableNotes[i]);
+    noOfNotes[i].innerHTML = notes;
+    amount %= availableNotes[i];
+  }
+}
 
+function validateBillAndCash() {
   hideMessage();
   if (billAmount.value >= 0) {
-
-    if (cashGiven.value >= billAmount.value) {
-      console.log("asdas");
+    if (Number(cashGiven.value) >= Number(billAmount.value)) {
+      calculate(Number(cashGiven.value) - Number(billAmount.value));
     } else {
       showMessage("Do you want to wash the dishes?");
     }
-
-
   } else {
     showMessage("Bill amount should be greater than 0.");
   }
